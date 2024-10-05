6bddabcb5c52e3aa8abec92040d3012263a98534
Alperen <alperenmimarlar@gmail.com>
2024-10-05 13:02:24 +0300

    Refactor code structure, update styling, and remove unused dependencies

---
+++ src/contexts/Context.js
@@ -0,0 +1,7 @@
+import { useContext } from "react";
+
+import React from "react";
+
+function Context() {}
+
+export default Context;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,7 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      blue: "#1fb6ff",
-      black: "#7e5bef",
-      gray: "#333",
-      lilac: "#8a3ffc",
-      white: "#fff",
-      lightGray: "#f3f4f6",
-      green: "#0dd664",
-      purple: "#7e5bef",
-      red: "#ff4d4d",
-    },
     extend: {},
   },
-  plugins: [require("daisyui")],
-  daisyui: {
-    themes: ["light", "dark"],
-  },
+  plugins: [],
 };
